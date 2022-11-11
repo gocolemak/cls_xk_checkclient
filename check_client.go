b14fd8cd59a7f6e55663b826d158c93bdd8d2f39
gocolemak <colemak@126.com>
2022-11-11 16:17:10 +0800

    modify user datatype to int

--- check_client.go
+++ check_client.go
@@ -15,19 +15,19 @@ type Reply struct {
 
 type CheckClient struct {
 	Address   string
-	user      string
+	user      int
 	checkType string
 	isSync    int
 }
 
-func (c CheckClient) CallCheckAsync(user string, checkType string) (Reply, error) {
+func (c CheckClient) CallCheckAsync(user int, checkType string) (Reply, error) {
 	c.user = user
 	c.checkType = checkType
 	c.isSync = 0
 	return c.callCheckHandler()
 }
 
-func (c CheckClient) CallCheckSync(user string, checkType string) (Reply, error) {
+func (c CheckClient) CallCheckSync(user int, checkType string) (Reply, error) {
 	c.user = user
 	c.checkType = checkType
 	c.isSync = 1
@@ -37,15 +37,12 @@ func (c CheckClient) CallCheckSync(user string, checkType string) (Reply, error)
 func (c CheckClient) callCheckHandler() (Reply, error) {
 	// 1. 参数校验并解析
 	// 1.1 判空,不支持为空, 为空则直接返回
-	if c.user == "" {
-		return Reply{Status: 0}, errors.New("用户id不能为空")
-	}
 	if c.checkType == "" {
 		return Reply{Status: 0}, errors.New("检查项标识不能为空")
 	}
 	// 2. 服务发现 c.address
 	// 远程调用 check_work , 发送rpc or http
-	url := "http://" + c.Address + "/xk-api/checkwork/do?user=" + c.user + "&check_type=" + c.checkType + "&is_sync=" + fmt.Sprintf("%d", c.isSync)
+	url := "http://" + c.Address + "/xk-api/checkwork/do?user=" + fmt.Sprintf("%d", c.user) + "&check_type=" + c.checkType + "&is_sync=" + fmt.Sprintf("%d", c.isSync)
 	client := &http.Client{}
 	//提交请求
 	reqest, err := http.NewRequest("GET", url, nil)
